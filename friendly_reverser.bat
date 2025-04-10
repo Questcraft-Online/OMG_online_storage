4c664daa0b486bb8ed19100d09716ba9ffbda24a
triggerhappy_ <questcraft.online@gmail.com>
2025-04-10 20:57:28 +0200

    Update friendly_reverser.bat

--- friendly_reverser.bat
+++ friendly_reverser.bat
@@ -1,8 +1,6 @@
 @echo off
 echo Reversing prank...
 DEL "%USERPROFILE%\Desktop\You_are_hacked*.jpg"
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d " /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 echo All cleaned up!
 echo self destruct
 DEL "%USERPROFILE%\Desktop\friendly_reverser.bat"
