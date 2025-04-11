74581f81000f216afd592ea76830abf965c6c46f
triggerhappy_ <questcraft.online@gmail.com>
2025-04-11 13:47:29 +0200

    Update friendly_reverserV2.bat

--- friendly_reverserV2.bat
+++ friendly_reverserV2.bat
@@ -1,1 +1,16 @@
-test
+@echo off
+echo finding divice language...
+for /f "tokens=2 delims=:" %%a in ('systeminfo ^| find "System Locale:"') do set locale=%%a
+set locale=%locale: =%
+if /I "%locale:~0,5%"=="nl-NL" (
+    set startscreen=Bureaublad
+) else (
+    set startscreen=Desktop
+)
+
+set "dest=%USERPROFILE%\%startscreen%"
+echo Reversing Damage...
+DEL "%dest%\you_are_hacked*.jpg"
+echo All cleaned up!
+echo self destruct
+DEL "%dest%\friendly_reverser.bat"
